b7731a95d6592aa5a662a274fb5932d6ff09c049
Brian W Wiatrek <brianwiatrek@aol.com>
2024-08-31 18:51:55 -0500

    Updated main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+/*
+Short Description:  This program will convert Imperial distance to Metric measurements.
+Author:  Brian Wiatrek
+Date:  August 20, 2024
+*/
 public class Main {
+
+    public static class imperialMetricConversion {
+        public static double footToMeter(double foot){
+            return 0.305*foot;
+        }
+
+        public static double meterToFoot(double meter){
+            return 3.279 * meter;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.printf("%s%10s |  %s%7s\n", "Feet","Meters","Meters","Feet");
+        System.out.printf("--------------------------------\n");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%-3d%10.2f  |  %d%12.2f\n", i, imperialMetricConversion.footToMeter(i), (5*(3+i)),
+                    imperialMetricConversion.meterToFoot((5*(3+i))));
+        }
     }
 }
